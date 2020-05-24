6aae67b381c5469ba4bd4639b9c424a0defb72f5
Nithin Rauj <nithindrake@gmail.com>
2020-05-24 20:24:01 +0530

    Display posts in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Post from '../components/Post';
 
-const Home = () => {
-  return <div>Home Stories</div>;
-};
+class Home extends Component {
+  state = {
+    posts: [],
+  };
+  async componentDidMount() {
+    const res = await fetch(
+      'https://hacker-news.firebaseio.com/v0/topstories.json?'
+    );
+    let ids = await res.json();
+    ids = ids.slice(0, 20);
+    ids.map(async (id) => {
+      const res = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      );
+      const data = await res.json();
+      this.setState({ posts: [...this.state.posts, data] });
+    });
+  }
+  render() {
+    return (
+      <div className='posts-container'>
+        {this.state.posts &&
+          this.state.posts.map((post) => <Post key={post.id} post={post} />)}
+      </div>
+    );
+  }
+}
 
 export default Home;
