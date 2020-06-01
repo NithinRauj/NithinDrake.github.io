7e23d530ca1974bf7ecff7d0658d31d4250a8dad
Nithin Rauj <nithindrake@gmail.com>
2020-06-01 21:41:23 +0530

    Updates

---
+++ precache-manifest.37f5145b61ff053a4b27fffac494ad99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc363d74a52c42ab6740cf3a4258d65",
+    "url": "/react-hackernews-clone/index.html"
+  },
+  {
+    "revision": "a205241cded87dbf9d86",
+    "url": "/react-hackernews-clone/static/css/main.524835ea.chunk.css"
+  },
+  {
+    "revision": "f1d1230b062e2104b5da",
+    "url": "/react-hackernews-clone/static/js/2.a16d9a23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hackernews-clone/static/js/2.a16d9a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a205241cded87dbf9d86",
+    "url": "/react-hackernews-clone/static/js/main.caec1dd1.chunk.js"
+  },
+  {
+    "revision": "f9d909a3a8f71614f6d9",
+    "url": "/react-hackernews-clone/static/js/runtime-main.64c51f4a.js"
+  }
+]);
